950f704a774c378446c70a1d31dfe328096eea5b
Cem Bıçakcı <cmbicakci@gmail.com>
2024-01-06 14:29:32 +0300

    initial commit

---
+++ movie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  movie
+//
+//  Created by Cem Bıçakcı on 6.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ movie/movieApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieApp.swift
+//  movie
+//
+//  Created by Cem Bıçakcı on 6.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct movieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
